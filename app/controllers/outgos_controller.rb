a27ec17572cf2ebc2e2bb41179e5cacc50762a6c
totopiro <rockey0825@i.softbank.jp>
2023-01-09 20:42:35 +0900

    feature_genre

--- app/controllers/outgos_controller.rb
+++ app/controllers/outgos_controller.rb
@@ -13,12 +13,27 @@ class OutgosController < ApplicationController
   def index
     @outgos = current_user.outgos
     @cost = 0
+    @asset = 0
+    @outgos.each do |outgo|
+      @asset -= outgo.cost
+    end
     if params[:that_day]
       @outgo_day = Outgo.where(start_time: params[:that_day])
     end
   end
 
+  def show
+    @outgo = Outgo.find(params[:id])
+  end
+
   def edit
+    @outgo = Outgo.find(params[:id])
+  end
+
+  def update
+    @outgo = Outgo.find(params[:id])
+    @outgo.update(outgo_params)
+    redirect_to outgos_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
   resources :salaries, only: [:index, :edit, :show]
-  resources :genres, only: [:index]
-  resources :outgos, only: [:new, :index, :edit, :create]
+  resources :genres, only: [:index, :create, :edit, :update]
+  resources :outgos, only: [:new, :index, :edit, :show, :create, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
